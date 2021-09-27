becf6562f15803b6260a3830f0e03b0e5c47af8e
kirkol <antonina.kirkalo@gmail.com>
2021-09-27 15:13:57 +0200

    doc generated

--- albis-leasing-sdk/howTo.js
+++ albis-leasing-sdk/howTo.js
@@ -10,5 +10,5 @@ If you'd like to create a doc run:
 
 jsdoc -d ../public/doc/ src/index.js to create a documentation for this file
 OR if .md file needed
-jsdoc2md src/index.js  > yourFile.md to create a .md version of documentation (for GitHub)
+jsdoc2md src/index.js  > README.md to create a .md version of documentation (for GitHub or docusaurus)
 */
